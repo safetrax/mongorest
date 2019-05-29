8b2a81d95f8fb265e6c68f40c2e09b17b796342a
vidya sagar <vidya.sagar@mtap.in>
2019-05-29 19:47:38 +0530

    fixed indentation issues

--- mongoser/src/main/java/in/mtap/iincube/mongoser/handlers/DataBaseAccessChecker.java
+++ mongoser/src/main/java/in/mtap/iincube/mongoser/handlers/DataBaseAccessChecker.java
@@ -6,16 +6,17 @@ public class DataBaseAccessChecker implements RequestInterceptor {
   private List<String> readRestrictedCollections;
   private List<String> writeRestrictedCollections;
 
-  public DataBaseAccessChecker (List<String> readRestrictedCollections, List<String> writeRestrictedCollections){
-      this.readRestrictedCollections = readRestrictedCollections;
-      this.writeRestrictedCollections = writeRestrictedCollections;
+  public DataBaseAccessChecker(List<String> readRestrictedCollections,
+                                List<String> writeRestrictedCollections) {
+    this.readRestrictedCollections = readRestrictedCollections;
+    this.writeRestrictedCollections = writeRestrictedCollections;
   }
 
   @Override public boolean isReadRestricted(String dbName, String colName) {
-      return readRestrictedCollections.contains(colName);
+    return readRestrictedCollections.contains(colName);
   }
 
   @Override public boolean isWriteRestricted(String dbName, String colName) {
-      return writeRestrictedCollections.contains(colName);
+    return writeRestrictedCollections.contains(colName);
   }
 }
